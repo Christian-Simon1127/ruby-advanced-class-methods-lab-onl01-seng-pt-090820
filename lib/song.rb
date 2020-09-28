7d82b0d742575a13905fb4323d5f8b0cb6946d53
Christian Simon <csimon1127@gmail.com>
2020-09-28 20:17:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,55 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = Song.new
+    @@all << song
+    song
+  end
+  
+  def self.new_by_name(name)
+    self.create
+    @name = name 
+  end
+  
+  def self.create_by_name(name) 
+    self.create
+    @name = name 
+    self.save
+    self
+  end
+  
+  def self.find_by_name(name)
+    self.all.find{|song| song.name == name}
+  end
+  
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name)
+      true
+    else 
+      self.create_by_name(name)
+    end
+  end 
+  
+  def self.alphabetical
+    self.all.sort
+  end
+  
+  def self.new_from_filename(file_name)
+    names = file_name.split(" - ")
+    more_names = names[1].split(".") 
+    @artist_name = names[0]
+    @name = more_names[1]
+  end
+  
+  def self.create_from_filename(file_name)
+    self.new_from_filename(file_name)
+    self.save
+  end
+  
+  def self.destroy_all
+    self.all.clear
+  end
+  
+  
 end
